3a88675fee02e4d751d70610e474198485076f9c
Micah Young <myoung@pivotal.io>
2017-11-01 13:06:53 -0400

    Add express

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const puppeteer = require('puppeteer');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-(async () => {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({path: 'example.png'});
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
-    await browser.close();
-})();
+app.get('/take', function(req, res){
+  (async () => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+      const page = await browser.newPage();
+      await page.goto('https://example.com');
+      await page.screenshot({path: 'public/image.png'});
 
+      await browser.close();
+
+      res.redirect('/static/image.png');
+  })();
+});
+
+app.listen(process.env.PORT || 3000)
